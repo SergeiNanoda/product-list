730d47f54745ee1346382918a15eac271c6d3a08
Sergei Osadchev <sergeiosadchev89@gmail.com>
2023-08-07 16:18:22 +0200

    Add favourite counter

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,10 +3,13 @@ import "./header.scss";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { toggleFavouritesVisible } from "../../state";
+import { useGlobalState } from "../../state";
 
 function Header() {
   const navigate = useNavigate();
   const location = useLocation();
+  const [favouriteProducts] = useGlobalState("favourites");
+  const isVisible = favouriteProducts.length ? "visible" : "hidden";
 
   const path = useMemo(() => {
     return location.pathname.replace(/(\/product\/)(\d+)/, "$1:id");
@@ -25,12 +28,11 @@ function Header() {
         className="fa-solid fa-house"
         style={{ color: "#0a0a0a" }}
       ></i>
-      <div className="favouriteItemsButton">
-        <i
-          onClick={toggleFavouritesVisible}
-          className="fa-solid fa-heart"
-          style={{ color: "#000000" }}
-        ></i>
+      <div className="favouriteItemsButton" onClick={toggleFavouritesVisible}>
+        <span className={`favouriteCount ${isVisible}`}>
+          {favouriteProducts.length}
+        </span>
+        <i className="fa-solid fa-heart" style={{ color: "#000000" }}></i>
       </div>
       <span className="header__title">{pageTitles[path]}</span>
     </div>
